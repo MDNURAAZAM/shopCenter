3b74d5ce95d3a52f305f222eeab0ff7ae91c5fc5
MDNURAAZAM <md.nura.azam@g.bracu.ac.bd>
2024-04-18 23:23:51 +0600

    add home page

---
+++ app/components/Product/Product.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Product = ({ product }) => {
+  const {
+    id,
+    title,
+    description,
+    price,
+    category,
+    discountPercentage,
+    thumbnail,
+  } = product || {};
+
+  const discountPrice = Math.round(price - price * (discountPercentage / 100));
+  return (
+    <div>
+      <div style={{background: `url(${thumbnail})`}} className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
+      <h2 className="text-sm lg:text-base mt-2">
+        <a className="text-base font-bold" href="./productPage.html">
+          {title}
+        </a>
+        <span className="text-[#919090]">
+          <a href="./category.html">({category})</a>
+        </span>
+      </h2>
+      <p className="text-[#919090] text-sm ">{description}</p>
+
+      <p className="text-rose-600 text-sm mt-4">
+        <span className="text-[#919090] line-through">${price}</span> $
+        {discountPrice}
+      </p>
+    </div>
+  );
+};
+
+export default Product;
